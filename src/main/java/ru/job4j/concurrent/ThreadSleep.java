b2c0a7960ca95fc26006101aa6d41e6f0f24d800
Sergey Manannikov <s_manannikov@mail.ru>
2021-01-13 19:37:57 +0300

    4. Скачивание файла с ограничением.

---
+++ src/main/java/ru/job4j/concurrent/ThreadSleep.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class ThreadSleep {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Start loading ... ");
+                        for (int i = 0; i <= 100; i++) {
+                            System.out.print("\rLoading : " + i + "%");
+                            Thread.sleep(300);
+                        }
+                        System.out.println(System.lineSeparator() + "Loaded.");
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,22 +1,50 @@
 package ru.job4j.concurrent;
 
-public class Wget {
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        System.out.println("Start loading ... ");
-                        for (int i = 0; i <= 100; i++) {
-                            System.out.print("\rLoading : " + i + "%");
-                            Thread.sleep(300);
-                        }
-                        System.out.println(System.lineSeparator() + "Loaded.");
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
+public class Wget implements Runnable {
+    private final String url;
+    private final String destination;
+    private final int speed;
+
+    public Wget(String url, String destination, int speed) {
+        this.url = url;
+        this.destination = destination;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(destination)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long startTime = System.nanoTime();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                long endTime = System.nanoTime();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long elapsedTime = endTime - startTime / 1000000000;
+                long finalTime = speed * 100L;
+                if (elapsedTime < finalTime) {
+                    Thread.sleep(finalTime - elapsedTime);
                 }
-        );
-        thread.start();
+            }
+        } catch (
+                IOException | InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        String destination = args[1];
+        int speed = Integer.parseInt(args[2]);
+        Thread wget = new Thread(new Wget(url, destination, speed));
+        wget.start();
+        wget.join();
     }
 }
