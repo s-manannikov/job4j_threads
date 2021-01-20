6bb6e29bb10f875daac2a55692d7e5af32677235
Sergey Manannikov <s_manannikov@mail.ru>
2021-01-21 00:11:05 +0300

    0. CAS - операции.

--- src/main/java/ru/job4j/cas/CASCount.java
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -2,26 +2,21 @@ package ru.job4j.cas;
 
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount<T> {
-    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+    private final AtomicInteger count = new AtomicInteger(0);
 
     public void increment() {
         int value;
-        if (count.get() == null) {
-            throw new UnsupportedOperationException("Count is not impl.");
-        }
         do {
             value = count.get();
         } while (!count.compareAndSet(value, ++value));
     }
 
     public int get() {
-        if (count.get() == null) {
-            throw new UnsupportedOperationException("Count is not impl.");
-        }
         return count.get();
     }
 }
--- src/test/java/ru/job4j/cas/CASCountTest.java
+++ src/test/java/ru/job4j/cas/CASCountTest.java
@@ -11,7 +11,25 @@ public class CASCountTest {
     @Test
     public void whenIncrementAndGet() {
         CASCount<Integer> count = new CASCount<>();
-        IntStream.rangeClosed(0, 100).forEach(i -> new Thread(count::increment).start());
+        for (int i = 0; i <= 100; i++) {
+            new Thread(count::increment).start();
+        }
         assertEquals(count.get(), 100);
+    }
+
+    @Test
+    public void when2Threads() throws InterruptedException {
+        CASCount<Integer> count = new CASCount<>();
+        Thread t1 = new Thread(() -> {
+            IntStream.range(0, 100).forEach(i -> count.increment());
+        });
+        Thread t2 = new Thread(() -> {
+            IntStream.range(0, 100).forEach(i -> count.increment());
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        assertEquals(count.get(), 200);
     }
 }
